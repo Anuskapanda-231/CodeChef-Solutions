751b7bda99f52c2165d0fb23f273eb70998943c1
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-08 11:27:02 +0530

    ✅ SEARCHINARR - Rating 600 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-08

---
+++ codechef/easy/SEARCHINARR---Rating-600/solution.java
@@ -0,0 +1,13 @@
+public static String solve(int N, int X, int[] A) {
+    
+    for (int i=0;i<A.length;i++){
+        if(A[i]==X){
+            return "YES" ;
+        }
+        
+    }
+    return "NO";
+    
+    
+        
+}
