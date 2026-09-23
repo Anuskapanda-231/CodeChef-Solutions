86ceb8939344cb5f5565edf55bb6bd9c666b3d6d
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-23 11:37:19 +0530

    ✅ PARKINGCHARG | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/PARKINGCHARG/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc =  new Scanner(System.in);
+        
+        int X = sc.nextInt(),Y=sc.nextInt();
+        
+        int maxcus = X*Y ;
+        
+        System.out.println(maxcus);
+	}
+}
