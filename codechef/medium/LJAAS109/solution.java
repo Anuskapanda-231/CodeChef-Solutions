6a5e46a481356deaad4a43db126cfd633f801753
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-23 12:43:38 +0530

    ✅ LJAAS109 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LJAAS109/solution.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+       int i =1;
+       
+       while(i<=5){
+           System.out.println(i*i);
+           i++;
+       }
+	}
+}
