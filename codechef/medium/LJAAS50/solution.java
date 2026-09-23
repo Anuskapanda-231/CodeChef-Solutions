361686fd089311c7d54aa2703ec92e50744a3feb
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-23 12:19:16 +0530

    ✅ LJAAS50 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LJAAS50/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+       Scanner sc = new Scanner(System.in);
+       
+       String a = sc.nextLine();
+       String b = sc.nextLine();
+       
+       System.out.println( a  +" " + b);
+       }
+}
