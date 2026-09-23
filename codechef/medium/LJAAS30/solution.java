7b2775e5f254903f27d98ee3c64a4b964de7c062
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-23 11:53:48 +0530

    ✅ LJAAS30 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LJAAS30/solution.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here
+         double temperature = 25.5 ;
+         
+         double kelvin = 273 + temperature ;
+         
+         System.out.println("Celsius -" + temperature + "\nKelvin -" + kelvin);
+    }
+}
