1d2a7837a11d14095b3d7148d8a49244d5a83c3d
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-23 12:16:49 +0530

    ✅ LJAAS49 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LJAAS49/solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        
+        double pi = 3.14 ;
+        
+        int r =sc.nextInt();
+        
+        System.out.println(pi*r*r);
+	}
+}
