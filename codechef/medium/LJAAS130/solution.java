db8703603cc950ea0f9db8c55dd1d991f266d738
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-23 12:52:14 +0530

    ✅ LJAAS130 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/LJAAS130/solution.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		int[] values = {2, 10, 12, 6, 15};
+        // Complete the code 
+         for(int i =0; i<values.length;i++){
+             if(values[i]<=10){
+                 System.out.print(values[i]*values[i] +" ");
+             }
+         }   
+    }
+}
