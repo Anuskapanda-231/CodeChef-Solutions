a5fb9c60ffea0e66266f7daba4b9cefb5c176288
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-25 18:08:50 +0530

    ✅ FTTM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/FTTM/solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{   
+    
+    
+    public static char getmostfreqletter(String s){
+        
+        int[] freq = new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            char ch = Character.toLowerCase(s.charAt(i));
+            
+            if(Character.isLetter(ch)){
+                freq[ch-'a']++;
+            }
+        }
+        
+        int maxval=0;
+        char answer ='a';
+        for(int i=0;i<26;i++){
+            if(freq[i]>maxval){
+                maxval=freq[i];
+                answer =(char) (i +'a');
+            }
+        }
+        return answer;
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		System.out.println(getmostfreqletter(s));
+
+	}
+}
