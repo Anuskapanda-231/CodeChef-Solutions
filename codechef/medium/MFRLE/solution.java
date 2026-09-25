ae3c312fb4c0066e2d63b29c23f6085bbdbf2724
Anuskapanda-231 <pandaanuska231@gmail.com>
2026-09-25 11:59:25 +0530

    ✅ MFRLE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/MFRLE/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.nextLine();
+		
+		HashMap<String,Integer> map = new HashMap<>();
+		
+	    for(int i =0; i<S.length()-1;i++){
+	        String pair =S.substring(i,i+2);
+	        
+	        map.put(pair,map.getOrDefault(pair,0)+1);
+	    }	
+        
+        int count =0 ;
+        
+        for(int num : map.values()){
+            if(num>=2){
+                count++;
+            }
+        }
+        System.out.println(count);
+	}
+}
